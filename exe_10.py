106b8bec79450a22888ba65fd11b50d66b9a23b8
davi-bernardojr <89274175+davi-bernardojr@users.noreply.github.com>
2022-06-13 17:13:16 -0300

    Update exe_10.py

--- exe_10.py
+++ exe_10.py
@@ -1,9 +1,11 @@
 def triangulo(total_Linhas):
     content = []
 
+    #cria a lista com o numero de linhas informado pelo usuário
     for count_1 in range(1, total_Linhas+1):
         content.append([])
         
+    #atribui a cada linha o numero de espaços referentes a sua linha
     for count_1 in range(len(content)):
         for count_2 in range(count_1+1):
             content[count_1].append([])
@@ -16,7 +18,8 @@ def triangulo(total_Linhas):
                 content[count_1][count_2] = 1
             else:
                 content[count_1][count_2] = content[count_1-1][count_2-1] + content[count_1-1][count_2]
-
+                
+    #exibe o triangulo
     for count_1 in range(len(content)):
         print(f"Linha {count_1+1} - {content[count_1]}")
 
